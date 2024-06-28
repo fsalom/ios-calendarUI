01c64730b6ac380581b1248e4e53e6f1ba64936f
Fernando Salom <fer@rudo.es>
2024-06-28 14:57:18 +0200

    Add delegate

---
+++ Sources/Delegate/CalendaUIDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Fernando Salom Carratala on 25/6/24.
+//
+
+import Foundation
+
+public protocol CalendarUIDelegate {
+    func clickOn(day: (Day)->())
+}
