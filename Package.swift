e94c71575c28feae9c073d7ab66950633a25021b
Fernando Salom <fer@rudo.es>
2024-08-03 23:11:24 +0200

    Fix package properties

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "calendarUI",
+    name: "CalendarUI",
     platforms: [ .iOS(.v15) ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "calendarUI",
-            targets: ["calendarUI"]),
+            name: "CalendarUI",
+            targets: ["CalendarUI"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "calendarUI",
+            name: "CalendarUI",
             path: "Sources/"),
         .testTarget(
             name: "calendarUITests",
-            dependencies: ["calendarUI"],
+            dependencies: ["CalendarUI"],
             path: "Tests/"),
     ]
 )
