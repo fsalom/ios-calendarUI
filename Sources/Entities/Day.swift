ad66b84649d9f04a50b0183545065d724442a450
Fernando Salom <fer@rudo.es>
2024-08-06 15:17:32 +0200

    Add date to Day entity

--- Sources/Entities/Day.swift
+++ Sources/Entities/Day.swift
@@ -2,6 +2,20 @@ import Foundation
 
 public struct Day: Identifiable {
     public let id = UUID()
+    var month: Date? = nil
     let number: Int
     let isWithinDisplayedMonth: Bool
+    var date: Date {
+        guard let month else { return Date() }
+        let calendar = Calendar.current
+        var dateComponents = calendar.dateComponents([.year, .month, .day], from: month)
+        dateComponents.day = self.number
+        return calendar.date(from: dateComponents) ?? Date()
+    }
+
+    init(number: Int, month: Date? = nil, isWithinDisplayedMonth: Bool) {
+        self.month = month
+        self.number = number
+        self.isWithinDisplayedMonth = isWithinDisplayedMonth
+    }
 }
