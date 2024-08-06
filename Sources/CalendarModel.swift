c5b403b12e439200524d4a47a73927ea1f9064ce
Fernando Salom <fer@rudo.es>
2024-08-06 15:18:34 +0200

    Add model new features

--- Sources/CalendarModel.swift
+++ Sources/CalendarModel.swift
@@ -7,11 +7,16 @@ open class CalendarModel {
         case monday = 2
         case sunday = 1
     }
+    public enum CalendarType {
+        case calendar
+        case full
+    }
 
     private let weekDays = ["Lun", "Mar", "Mie", "Jue", "Vie", "Sab", "Dom"]
     private let weekDaysSingle = ["L", "M", "X", "J", "V", "S", "D"]
     private let firstWeekDay: FirstWeekDay
     private let year: Int
+    let type: CalendarType
     let months: [Date] = {
         let calendar = Calendar.current
         let year = calendar.component(.year, from: Date())
@@ -25,7 +30,11 @@ open class CalendarModel {
         return dates
     }()
 
-    public init(firstWeekDay: FirstWeekDay, year: Int? = nil, delegate: CalendarUIDelegate? = nil) {
+    public init(type: CalendarType = .calendar,
+                firstWeekDay: FirstWeekDay = .monday,
+                year: Int? = nil,
+                delegate: CalendarUIDelegate? = nil) {
+        self.type = type
         self.firstWeekDay = firstWeekDay
         if let year {
             self.year = year
@@ -41,7 +50,7 @@ open class CalendarModel {
         let range = calendar.range(of: .day, in: .month, for: month)!
         let firstOfMonth = calendar.date(from: calendar.dateComponents([.year, .month], from: month))!
         let firstDayOfWeek = calendar.component(.weekday, from: firstOfMonth)
-        
+
         var days: [Day] = []
         switch firstWeekday {
         case .monday:
@@ -50,7 +59,7 @@ open class CalendarModel {
         case .sunday:
             days = (1..<firstDayOfWeek).map { _ in Day(number: 0, isWithinDisplayedMonth: false) }
         }
-        days += range.map { Day(number: $0, isWithinDisplayedMonth: true) }
+        days += range.map { Day(number: $0, month: month, isWithinDisplayedMonth: true) }
         while days.count % 7 != 0 {
             days.append(Day(number: 0, isWithinDisplayedMonth: false))
         }
@@ -58,11 +67,21 @@ open class CalendarModel {
         return days
     }
 
+    func getDays() -> [Day] {
+        var days: [Day] = []
+        for month in months {
+            var daysOfMonth = generateDays(for: month, with: self.firstWeekDay)
+            daysOfMonth.removeAll(where: {$0.number == 0})
+            days.append(contentsOf: daysOfMonth)
+        }
+        return days
+    }
+
     func monthName(from date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMMM"
         return dateFormatter.string(from: date)
-    }
+    }    
 
     func display() {
         delegate?.clickOn { day in
