943fb54b0050504240b5a0e7ad4560d6b72abb9c
Fernando Salom <fer@rudo.es>
2024-08-10 22:17:06 +0200

    Fix day properties

--- Sources/Entities/Day.swift
+++ Sources/Entities/Day.swift
@@ -3,16 +3,11 @@ import Foundation
 public struct Day: Identifiable {
     public let id = UUID()
     var month: Date? = nil
-    let number: Int
-    let isWithinDisplayedMonth: Bool
-    var date: Date {
-        guard let month else { return Date() }
-        let calendar = Calendar.current
-        var dateComponents = calendar.dateComponents([.year, .month, .day], from: month)
-        dateComponents.day = self.number
-        return calendar.date(from: dateComponents) ?? Date()
-    }
+    var number: Int = 0
+    var isWithinDisplayedMonth: Bool = false
+    var date: Date? = nil
     var isPast: Bool {
+        guard let date = date else { return false }
         return date < Date() ? true : false
     }
 
@@ -20,5 +15,18 @@ public struct Day: Identifiable {
         self.month = month
         self.number = number
         self.isWithinDisplayedMonth = isWithinDisplayedMonth
+        self.date = calculateDate()
+    }
+
+    init(date: Date) {
+        self.date = date
+    }
+
+    func calculateDate() -> Date {
+        guard let month else { return Date() }
+        let calendar = Calendar.current
+        var dateComponents = calendar.dateComponents([.year, .month, .day], from: month)
+        dateComponents.day = self.number
+        return calendar.date(from: dateComponents) ?? Date()
     }
 }
