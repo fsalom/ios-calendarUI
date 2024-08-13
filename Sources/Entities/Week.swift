b019271690e0724ba0a4c9720bab90094f270c65
Fernando Salom <fer@rudo.es>
2024-08-13 16:21:33 +0200

    Update week properties

--- Sources/Entities/Week.swift
+++ Sources/Entities/Week.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-public struct Week {
+public struct Week: Identifiable {
+    public let id = UUID().uuidString
     let days: [Day]
+    var isPast: Bool {
+        guard let date = days[0].date else { return false }
+        return date < Date() ? true : false
+    }
+
 }
